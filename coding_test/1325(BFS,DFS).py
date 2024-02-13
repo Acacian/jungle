8a529087e0e1401ea085e0ceb21509a641786cde
DOHA <koo9811@gmail.com>
2024-02-13 15:53:05 +0900

    dailytest

--- coding_test/1325(BFS,DFS).py
+++ coding_test/1325(BFS,DFS).py
@@ -33,39 +33,45 @@
 
 
 
-# from collections import deque
 # import sys
+# from collections import deque
 # input = sys.stdin.readline
- 
-# n, m = map(int, input().split())
-# relationship = [[] for _ in range(n + 1)]
- 
-# # A가 B를 신뢰한다. (노드로 표현하면 B는 A의 부모이다.)
-# for _ in range(m):
-#     A, B = map(int, input().split())
-#     relationship[B].append(A)
- 
+
+# n,m = map(int,input().split())
+
 # def bfs(start):
-#     q = deque()
-#     q.append(start)
-#     cnt = 0
- 
-#     visited = [False] * (n + 1)
-#     visited[start] = True
- 
-#     while q:
-#         cur = q.popleft()
-#         for next in relationship[cur]:
-#             if not visited[next]:
-#                 visited[next] = True
-#                 q.append(next)
-#                 cnt += 1
-#     return cnt
- 
-# result = []
-# for start in range(1, len(relationship)):
-#     result.append(bfs(start))
- 
-# for i in range(len(result)):
-#     if max(result) == result[i]:
-#         print(i + 1)
+# 	cnt = 1
+# 	queue = deque([start])
+# 	visit = [False for _ in range(n+1)]
+# 	visit[start] = True
+
+# 	while queue:
+# 		cur = queue.popleft()
+
+# 		for nx in graph[cur]:
+# 			if not visit[nx]:
+# 				visit[nx] = True
+# 				cnt += 1
+# 				queue.append(nx)
+
+# 	return cnt
+
+# graph = [[] for _ in range(n+1)]
+
+# for _ in range(m):
+# 	a,b = map(int,input().split())
+# 	graph[b].append(a)
+
+# maxCnt = 1
+# ans = []
+
+# for i in range(1,n+1):
+# 	cnt = bfs(i)
+# 	if cnt > maxCnt:
+# 		maxCnt = cnt
+# 		ans.clear()
+# 		ans.append(i)
+# 	elif cnt == maxCnt:
+# 		ans.append(i)
+
+# print(*ans)
